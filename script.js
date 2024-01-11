7856c78425acd974504b2a2a0b02ae705c7152d5
Dev-scott <sadoscott25@gmail.com>
2024-01-11 21:06:15 +0100

    refactor my code

--- script.js
+++ script.js
@@ -1,34 +1,61 @@
-function vigenereCipher(message, key) {
-    message = message.toUpperCase();
-    key = key.toUpperCase();
+function vigenereCipher(message, key, encrypt) {
+  message = message.toUpperCase();
+  key = key.toUpperCase();
 
-    let encryptedText = '';
+  let resultText = "";
 
-    for (let i = 0, j = 0; i < message.length; i++) {
-        const currentChar = message.charAt(i);
+  for (let i = 0, j = 0; i < message.length; i++) {
+    const currentChar = message.charAt(i);
 
-        if (currentChar >= 'A' && currentChar <= 'Z') {
-            const shift = key.charCodeAt(j) - 'A'.charCodeAt(0);
-            const encryptedChar = String.fromCharCode(((currentChar.charCodeAt(0) + shift - 'A'.charCodeAt(0)) % 26) + 'A'.charCodeAt(0));
+    if (currentChar >= "A" && currentChar <= "Z") {
+      const shift = key.charCodeAt(j) - "A".charCodeAt(0);
+      let processedChar;
 
-            encryptedText += encryptedChar;
-            j = (j + 1) % key.length;
-        } else {
-            encryptedText += currentChar;
-        }
+      if (encrypt) {
+        // Chiffrement
+        processedChar = String.fromCharCode(
+          ((currentChar.charCodeAt(0) + shift - "A".charCodeAt(0)) % 26) +
+            "A".charCodeAt(0)
+        );
+      } else {
+        // Déchiffrement
+        processedChar = String.fromCharCode(
+          ((currentChar.charCodeAt(0) - shift + 26 * 2) % 26) +
+            "A".charCodeAt(0)
+        );
+      }
+
+      resultText += processedChar;
+      j = (j + 1) % key.length;
+    } else {
+      // Caractère non alphabétiques
+      resultText += currentChar;
     }
-    
-    return encryptedText;
+  }
+
+  return resultText;
 }
 
 function encrypt() {
-    const messageInput = document.getElementById('message');
-    const keyInput = document.getElementById('key');
-    const resultOutput = document.getElementById('result');
+  const messageInput = document.getElementById("message");
+  const keyInput = document.getElementById("key");
+  const resultOutput = document.getElementById("result");
 
-    const message = messageInput.value;
-    const key = keyInput.value;
+  const message = messageInput.value;
+  const key = keyInput.value;
 
-    const encryptedMessage = vigenereCipher(message, key);
-    resultOutput.value = encryptedMessage;
+  const encryptedMessage = vigenereCipher(message, key, true);
+  resultOutput.value = encryptedMessage;
+}
+
+function decrypt() {
+  const messageInput = document.getElementById("message");
+  const keyInput = document.getElementById("key");
+  const resultOutput = document.getElementById("result");
+
+  const message = messageInput.value;
+  const key = keyInput.value;
+
+  const decryptedMessage = vigenereCipher(message, key, false);
+  resultOutput.value = decryptedMessage;
 }
